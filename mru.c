dc6e7b5f3150ca48829b2dcfc41fe8fe60047f1c
Facuvatti <facuvatti@gmail.com>
2025-03-27 12:52:49 -0300

    Agregué la formula correcta

--- mru.c
+++ mru.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main(int argc, char const *argv[]){
-    double d1 = atof(argv[1]);
-    double d2 = atof(argv[2]);
+    double di1 = atof(argv[1]);
+    double di2 = atof(argv[2]);
     double v1 = atof(argv[3]);
     double v2 = atof(argv[4]);
-    double t = (d2-d1)/(v2-v1);
+    double df = (-di1*v2+di2*v1)/(v1-v2);
+    double t = df/v1;
     printf("El tiempo de choque es: %lf",t);
 }
