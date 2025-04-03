8661206b00f6fe71f48257ee921c90347569fbe8
Facuvatti <facuvatti@gmail.com>
2025-04-03 11:33:08 -0300

    Hice el programa de bisiesto sin usar if, como queria el profe :)

--- bisiesto.c
+++ bisiesto.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 // %d -> decimal %s -> string | %c -> character | %f -> float %t -> boolean %o -> octal %u -> unsigned %ld -> long decimal %lld -> long long decimal %x -> hexadecimal 
 int main(int argc, char *argv[]) {
     int year = atoi(argv[1]);
-
-    if (year%4 == 0 && year%100 != 0 || year%400 == 0){
-        printf("El year %d es bisiesto", year);
-    }
-    else {
-        printf("El year %d no es bisiesto", year);
-    }
-    return 0;
+    int r = 0;
+    r = ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) ? 1:0; 
+    printf("%s",r == 1 ? "Es bisiesto" : "No es bisiesto" );
 }
